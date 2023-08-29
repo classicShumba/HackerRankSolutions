230a296673300358de2d1045e7bf8c3741828b84
[Allan Shumba] <[questykings@gmail.com]>
2023-08-29 17:20:34 +0200

    solved the basic if-else problem

--- IfElseHackerRank/src/Main.java
+++ IfElseHackerRank/src/Main.java
@@ -13,9 +13,9 @@ public class Main {
     public static void Solution(int N) {
         if (N % 2 != 0){
             System.out.println("Weird");
-        } else if (N % 2 == 0 && N > 2 && N < 5){
+        } else if (N % 2 == 0 && N >= 2 && N <= 5){
             System.out.println("Not Weird");
-        } else if (N % 2 == 0 && N > 6 && N < 20){
+        } else if (N % 2 == 0 && N >= 6 && N <= 20){
             System.out.println("Weird");
         } else if (N % 2 == 0 && N > 20){
             System.out.println("Not Weird");
